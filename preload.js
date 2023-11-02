1642348d02fc7b8568829cc2f369cd573e65f876
xujr37 <1156429007@qq.com>
2023-11-02 15:35:29 +0800

    test

--- preload.js
+++ preload.js
@@ -1,9 +1,9 @@
-const { contextBridge, ipcRenderer } = require("electron");
+// const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  // 除函数之外，我们也可以暴露变量
-  ping: () => ipcRenderer.invoke("ping"),
-});
+// contextBridge.exposeInMainWorld("versions", {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//   // 除函数之外，我们也可以暴露变量
+//   ping: () => ipcRenderer.invoke("ping"),
+// });
