a67b6c109b0e59271343c68fd46f3dd9d94dae3d
Cheri <1156429007@qq.com>
2023-11-07 00:41:48 +0800

    Enable loading URL instead of local file in createWindow function

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ const createWindow = () => {
       enableRemoteModule: true, // 可以使用remote方法
     },
   });
-  win.loadFile(path.resolve(__dirname, "./dist/index.html"));
-  // win.loadURL("http://localhost:8888");
+  // win.loadFile(path.resolve(__dirname, "./dist/index.html"));
+  win.loadURL("http://localhost:8888");
   // 打开开发工具
   win.webContents.openDevTools();
 };
